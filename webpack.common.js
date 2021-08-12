1716dfefe8d44c14b8ceaf4ec603eb2696f9471b
dianyehezkiel <dianyehezkiel@gmail.com>
2021-08-13 03:18:19 +0700

    install packages and add webpack & eslint config

---
+++ webpack.common.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /\.(scss)$/,
+      use: [{
+        loader: 'style-loader',
+      },
+      {
+        loader: 'css-loader',
+      },
+      {
+        loader: 'postcss-loader',
+        options: {
+          plugins: function() {
+            return [
+              require('autoprefixer'),
+            ];
+          },
+        },
+      },
+      {
+        loader: 'sass-loader',
+      },
+      ],
+    }],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      filename: 'index.html',
+    }),
+  ],
+};
