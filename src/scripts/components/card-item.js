b70cc8aa995ba7e4d7cc3bb82bc9510d29ad652b
dianyehezkiel <dianyehezkiel@gmail.com>
2021-10-04 17:18:48 +0700

    change card and carousel item to work with tv show data

--- src/scripts/components/card-item.js
+++ src/scripts/components/card-item.js
@@ -9,11 +9,11 @@ class CardItem extends HTMLElement {
     this.innerHTML = `
     <a href="#">
       <div class="card movie-card">
-        <img src="https://image.tmdb.org/t/p/w500${this._item.poster_path}" class="card-img-top" alt="${this._item.title} Poster">
+        <img src="https://image.tmdb.org/t/p/w500${this._item.poster_path}" class="card-img-top" alt="${this._item.title? this._item.title:this._item.name} Poster">
         <div class="card-body movie-card-body">
           <i class="bi bi-play-fill" data-toggle="tooltip" data-placement="bottom" title="Play Trailer"></i>
-          <h5 class="card-title movie-card-title">${this._item.title}</h5>
-          <span class="movie-info">${this._item.release_date.substr(0, 4)}</span>
+          <h5 class="card-title movie-card-title">${this._item.title? this._item.title:this._item.name}</h5>
+          <span class="movie-info">${this._item.release_date? this._item.release_date.substr(0, 4):this._item.first_air_date.substr(0, 4)}</span>
           <span class="movie-info movie-rating"><i class="bi bi-star-fill"></i> ${this._item.vote_average}</span>
         </div>
       </div>
