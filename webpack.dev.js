87763f83bb1ad6a1c7cfc7965bb2e4b1b42ae76a
dianyehezkiel <dianyehezkiel@gmail.com>
2021-10-07 03:58:59 +0700

    fix mistakes

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,4 +3,12 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 });
