aa39e3f20bbfc46883e628d362f6bfa8511edcc0
dianyehezkiel <dianyehezkiel@gmail.com>
2021-10-06 22:49:30 +0700

    Separate card item for movie and tv show

---
+++ src/scripts/components/movie-card.js
@@ -0,0 +1,32 @@
+/* eslint-disable max-len */
+class MovieCard extends HTMLElement {
+  set item(item) {
+    this._item = item;
+    this.render();
+  }
+
+  render() {
+    this.classList.add('card-item');
+    this.innerHTML = `
+    <a id="/movie/${this._item.id}">
+      <div class="card movie-card">
+        <img src="${this._item.poster_path? 'https://image.tmdb.org/t/p/w500' + this._item.poster_path : './src/assets/poster-fallback.jpg'}" 
+            class="shadow card-img-top" 
+            alt="${this._item.title.length? this._item.title:'-'} Poster">
+        <div class="card-body movie-card-body">
+          <h5 class="card-title movie-card-title">
+            ${this._item.title.length? this._item.title:'-'}
+          </h5>
+          <span class="movie-info">
+            ${this._item.release_date.length >= 4? this._item.release_date.substr(0, 4):'-'}
+          </span>
+          <span class="movie-info movie-rating">
+            <i class="bi bi-star-fill"></i> ${this._item.vote_average}
+          </span>
+        </div>
+      </div>
+    </a>`;
+  }
+}
+
+customElements.define('movie-card', MovieCard);
---
+++ src/scripts/components/tv-card.js
@@ -0,0 +1,32 @@
+/* eslint-disable max-len */
+class TvCard extends HTMLElement {
+  set item(item) {
+    this._item = item;
+    this.render();
+  }
+
+  render() {
+    this.classList.add('card-item');
+    this.innerHTML = `
+    <a id="/tv/${this._item.id}">
+      <div class="card movie-card">
+        <img src="${this._item.poster_path? 'https://image.tmdb.org/t/p/w500' + this._item.poster_path : './src/assets/poster-fallback.jpg'}" 
+            class="shadow card-img-top" 
+            alt="${this._item.name.length? this._item.name:'-'} Poster">
+        <div class="card-body movie-card-body">
+          <h5 class="card-title movie-card-title">
+            ${this._item.name.length? this._item.name:'-'}
+          </h5>
+          <span class="movie-info">
+            ${this._item.first_air_date.length >= 4? this._item.first_air_date.substr(0, 4):'-'}
+          </span>
+          <span class="movie-info movie-rating">
+            <i class="bi bi-star-fill"></i> ${this._item.vote_average}
+          </span>
+        </div>
+      </div>
+    </a>`;
+  }
+}
+
+customElements.define('tv-card', TvCard);
