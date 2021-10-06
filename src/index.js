7ddc0679b22358f6f73f74ecde064211e81463a8
dianyehezkiel <dianyehezkiel@gmail.com>
2021-10-07 03:35:48 +0700

    add loader and import image

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import '@splidejs/splide/dist/css/splide-core.min.css';
 import './styles/style.scss';
 import 'bootstrap/js/dist/collapse';
 import 'bootstrap/js/dist/dropdown';
+import './assets/favicon.png';
+import './assets/poster-fallback.jpg';
+import './assets/backdrop-fallback.jpg';
 import main from './scripts/main';
 
 document.addEventListener('DOMContentLoaded', main);
